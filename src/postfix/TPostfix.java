e7fc24b5dbe409b473c52842b3a774aff1b66f0d
Orozco Pablo <Orozco Pablo@C5DANIELOROZCO>
2014-07-24 17:36:40 -0600

    new commit - orozco -

--- src/postfix/TPostfix.java
+++ src/postfix/TPostfix.java
@@ -20,34 +20,58 @@ public class TPostfix {
     }
     
     public double eval(int n)
-    {
-        double a,b;
+    {        
         Stack se = new Stack(n);
-        int i = 0;
-        String error = "";
+        int i = 0;        
         for (i = 0; i < exp.length(); i++ )
         {
             switch (exp.charAt(i))
             {
-                case '0': se.push(exp.charAt(i)); break;
-                case '1': se.push(exp.charAt(i)); break;
-                case '2': se.push(exp.charAt(i)); break;
-                case '3': se.push(exp.charAt(i)); break;
-                case '4': se.push(exp.charAt(i)); break;
-                case '5': se.push(exp.charAt(i)); break;
-                case '6': se.push(exp.charAt(i)); break;
-                case '7': se.push(exp.charAt(i)); break;
-                case '8': se.push(exp.charAt(i)); break;
-                case '9': se.push(exp.charAt(i)); break;
+                case '0': se.push(0); break;
+                case '1': se.push(1); break;
+                case '2': se.push(2); break;
+                case '3': se.push(3); break;
+                case '4': se.push(4); break;
+                case '5': se.push(5); break;
+                case '6': se.push(6); break;
+                case '7': se.push(7); break;
+                case '8': se.push(8); break;
+                case '9': se.push(9); break;
                 case '+': 
                 {
-                    char o1 = se.pop();
-                    char o2 = se.pop();
-                    int n1 = Character.getNumericValue(o1);
-                    int n2 = Character.getNumericValue(o2);
-                    int n_res = n1 + n2;
-                    char temp_res = (char)n_res;
-                    se.push(temp_res);
+                    int o1 = se.pop();
+                    int o2 = se.pop();                    
+                    int n_res = o1 + o2;                                        
+                    se.push(n_res);
+                    break;
+                }
+                case '-':
+                {
+                    int o1 = se.pop();
+                    int o2 = se.pop();                    
+                    int n_res = -o1 + o2;                                        
+                    se.push(n_res);
+                    break;                   
+                }
+                case '*':
+                {
+                    int o1 = se.pop();
+                    int o2 = se.pop();                    
+                    int n_res = o1 * o2;                                        
+                    se.push(n_res);
+                    break;                    
+                }
+                case '/':
+                {
+                    int o1 = se.pop();
+                    int o2 = se.pop(); 
+                    int n_res = 0;
+                    if (o1 != 0)
+                        n_res = o2 / o1;
+                    else
+                        System.out.println("No se puede divir por 0");                    
+                    se.push(n_res);
+                    break;                    
                 }
             }
         }
