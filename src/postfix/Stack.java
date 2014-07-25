5c451b38f00941f9a35285a5c5f1a79eac557afb
Pablo <Pablo@192.168.1.3>
2014-07-24 21:55:08 -0600

    codigo comentado

--- src/postfix/Stack.java
+++ src/postfix/Stack.java
@@ -10,11 +10,14 @@ package postfix;
 
 
 public class Stack {
-    private int sp;
-    private int tamaño;
-    private int[] datos;
+    
+    private int sp; //atributo que representa el stack pointer de la pila
+    private int tamaño;//atributo que representa el tamaño total de la pila
+    private int[] datos;//atributo tipo vector para meter los valores en la pila
     
     public Stack()
+    //pre: nada
+    //post: inicia la pila con tamaño de 100 y apunta al proximo valor mas grande
     {
         datos = new int[100];
         sp = 99;
@@ -22,13 +25,19 @@ public class Stack {
     }
     
     public Stack(int n)
+   /**pre:constructor que recibe un entero no negativo
+    * post:inicia la pila con el tamaño ingresado y coloca el valor al puntero
+    */
     {
         datos = new int[n];
         sp = n-1;
         tamaño = n;
     }
     
-    public boolean empty()
+    public boolean isempty()
+    /**
+    * post: regresa true si hay un valor en el puntero actual, falso en otro caso
+    */
     {
         if (this.sp == this.tamaño)
             return true;
@@ -37,6 +46,10 @@ public class Stack {
     }
     
     public int top()
+    /**
+    * post: regresa el valor de hasta arriba de la pila, si esta vacio 
+    * manda un caracter vacio
+    */
     {
         if (this.sp < this.tamaño)
             return datos[sp];        
@@ -45,6 +58,9 @@ public class Stack {
     }
     
     public int pop()
+    /**
+    * post: regresa como entero el valor el valor que esta apuntando el puntero
+    */
     {
         int temp = 0;
         if ((this.sp < this.tamaño) && (this.sp > 0))
@@ -61,6 +77,10 @@ public class Stack {
     }
     
     public void push(int c)
+    /**pre: recibe un entero como el valor a meter en la pila
+    * post: asigna el valor en la posicion segun el puntero actual 
+    * en caso de llegar a la ultima posicion de la pila se muestra el mensaje de error
+    */
     {
         if ((this.sp < this.tamaño) && (this.sp >0))
         {           
