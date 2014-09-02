9ddc208147e28550f14a6f7ffe040b0c48282515
Pablo <Pablo@172.17.10.89>
2014-09-02 07:15:45 -0600

    -Sdx

---
+++ MainSET/src/mainset/MainSET.java
@@ -0,0 +1,21 @@
+/**
+* Universidad del Valle de Guatemala
+* Pablo Diaz 13203
+*/
+
+package mainset;
+
+/**
+ *
+ * @author Pablo
+ */
+public class MainSET {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
